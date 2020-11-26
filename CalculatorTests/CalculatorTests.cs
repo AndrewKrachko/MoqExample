100eb49e6423c979d8d1fa2a48a7707689ad6a3a
AndrewKrachko <AndrewKrachko@gmail.com>
2020-11-26 23:27:02 +0300

    Output test

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -36,5 +36,22 @@ namespace CalculatorTests
             // Assert
             Assert.AreEqual(50, sut.Object.Evaluate());
         }
+
+        [Test]
+        public void PresenterOutputTest()
+        {
+            // Arrange
+            var sut = new Mock<IPresenter>();
+            sut.Setup(s => s.Person).Returns("Sir");
+            sut.Setup(s => s.GetAnswer(It.IsAny<ICalculation>()))
+                .Returns($"Dear {sut.Object.Person}, here is your answer - 12.");
+            var calculation = new Mock<ICalculation>();
+
+            // Act
+            var output = sut.Object.GetAnswer(calculation.Object);
+
+            // Assert
+            Assert.AreEqual("Dear Sir, here is your answer - 12.", output);
+        }
     }
 }
