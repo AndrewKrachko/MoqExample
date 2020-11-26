d2607f0ca78d7b7f815ed61220f968bc73a4b75f
AndrewKrachko <AndrewKrachko@gmail.com>
2020-11-26 19:09:32 +0300

    Add project files.

---
+++ Calculator/ICalculation.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    public interface ICalculation
+    {
+        bool Verifiable { get; }
+        double Evaluate();
+        void GetA(double value);
+        void GetB(double value);
+    }
+}
