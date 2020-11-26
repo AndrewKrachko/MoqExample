3a0629fa7c308a7dfe0e15711aebb0396b3f4ed8
AndrewKrachko <AndrewKrachko@gmail.com>
2020-11-26 23:02:05 +0300

    Appended Geometry Interface for box

---
+++ Calculator/IBox.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    public interface IBox : IGeometryItem
+    {
+        double Width { get; set; }
+        double Height { get; set; }
+        double Depth { get; set; }
+        double Volume();
+    }
+}
---
+++ Calculator/Point3D.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculator
+{
+    public class Point3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Point3D() : this(0, 0)
+        {
+        }
+
+        public Point3D(double x, double y) : this(x, y, 0)
+        {
+        }
+
+        public Point3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
